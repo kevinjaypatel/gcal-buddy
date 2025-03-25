8ca3086d0aabfd7eaccf9da0a1fc832b09add9ce
kevinjaypatel <kevinjaypatel@gmail.com>
2025-03-25 14:29:00 -0700

    dev: started adding storage for keeping track of agent sessions

--- agent.py
+++ agent.py
@@ -1,13 +1,24 @@
 from agno.agent import Agent
+from agno.models.openai import OpenAIChat 
 from agno.tools.googlecalendar import GoogleCalendarTools
 import datetime
 import os
 from tzlocal import get_localzone_name
 
+# Storage 
+from agno.storage.postgres import PostgresStorage 
+
 credentials_path = "client_secret_126868835843-nmika7ldh47eqrmeln65h8vsaqh74ec5.apps.googleusercontent.com.json"
+agent_id = 'gpt-4o'
+db_url = 'postgresql+psycopg://ai:ai@localhost:5532/ai'
+
 agent = Agent(
-    tools=[GoogleCalendarTools(credentials_path=credentials_path)],
-    show_tool_calls=True,
+    name="Google Calendar Assistant",
+    model=OpenAIChat(id=agent_id),
+    storage=PostgresStorage(
+        table_name='agent_sessions', 
+        db_url=db_url, 
+    ),
     instructions=[
         f"""
         You are scheduling assistant . Today is {datetime.datetime.now()} and the users timezone is {get_localzone_name()}.
@@ -16,7 +27,11 @@ agent = Agent(
             - create events based on provided details
         """
     ],
+    tools=[GoogleCalendarTools(credentials_path=credentials_path)], 
     add_datetime_to_instructions=True,
+    show_tool_calls=True,
+    add_history_to_messages=True, 
+    debug_mode=True, 
 )
 
-agent.print_response("Give me the list of todays events", markdown=True)
+# agent.print_response("Give me the list of todays events", markdown=True)
